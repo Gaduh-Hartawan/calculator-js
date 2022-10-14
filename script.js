b14d4201e6393383d7d9889e15684afa2e3d2068
Gaduh Hartawan <gaduhhartaone@gmail.com>
2022-10-14 09:33:00 +0700

    Edit perulangan dan pengkondisian

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((item) => {
-    item.onclick = () => {
-      if (item.id == "clear") {
+buttons.forEach((button) => {
+    button.onclick = () => {
+      if (button.id == "clear") {
         display.innerText = "";
-      } else if (item.id == "backspace") {
+      } else if (button.id == "backspace") {
         let string = display.innerText.toString();
         display.innerText = string.substr(0, string.length - 1);
-      } else if (display.innerText != "" && item.id == "equal") {
+      } else if (display.innerText != "" && button.id == "equal") {
         display.innerText = eval(display.innerText);
-      } else if (display.innerText == "" && item.id == "equal") {
+      } else if (display.innerText == "" && button.id == "equal") {
         display.innerText = "Empty!";
         setTimeout(() => (display.innerText = ""), 2000);
       } else {
-        display.innerText += item.id;
+        display.innerText += button.id;
       }
     };
   });
-
-const themeToggleBtn = document.querySelector('.theme-toggler')
-const calculator = document.querySelector('.calculator')
-const toggleIcon = document.querySelector('.toggler-icon')
-let isDark = true
-themeToggleBtn.onclick = () => {
-    calculator.classList.toggle("dark");
-    themeToggleBtn.classList.toggle("active");
-    isDark = !isDark;
-  };
